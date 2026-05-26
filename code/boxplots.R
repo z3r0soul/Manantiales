c5e5c176196e9fa36847cbd509f93695fda720a1
z3r0soul <santiagovargas9@outlook.com>
2026-05-26 15:18:42 -0500

    Corrección comentariado y análisis de percentiles
    Se añadieron comentarios y outputs de strings con los diferentes titulos y procesos que se están realizando.

--- code/boxplots.R
+++ code/boxplots.R
@@ -2,9 +2,41 @@
 # 9. DIAGRAMAS DE CAJA Y BIGOTES
 # =============================================================
 
+cat("\n=============================================================\n
+    Generando percentiles y boxplots...
+    \n=============================================================\n" )
+
 # 9.1 Box plot provisional para practicar
 # Variable: pH del agua   
-box1 <- datos %>%
+# Hallar percentiles
+
+cat("\n === Resumen del diagrama de caja de Clasificación vs pH de laboratorio ===\n")
+boxplot1_summarise <- datos_top3 %>%
+  
+  filter(
+    !is.na(CLASIFICACION_LIMPIA),
+    !is.na(PH_LABORATORIO)
+  ) %>%
+  
+  group_by(CLASIFICACION_LIMPIA) %>%
+  
+  summarise(
+    
+    Q1 = quantile(PH_LABORATORIO, 0.25),
+    
+    Mediana = quantile(PH_LABORATORIO, 0.50),
+    
+    Q3 = quantile(PH_LABORATORIO, 0.75),
+    
+    RIQ = IQR(PH_LABORATORIO)
+    
+    )
+
+# Impresión de los percentiles del boxplot 1 
+print(boxplot1_summarise)
+
+# Creación del boxplot1  
+box1 <- datos_top3 %>%
   filter(!is.na(CLASIFICACION_LIMPIA),
          !is.na(PH_LABORATORIO)
   ) %>%
@@ -14,7 +46,7 @@ box1 <- datos %>%
   geom_boxplot() +
   guides(fill = "none") +
   labs(
-    title = "Distribución del pH por tipo de agua",
+    title = "Distribución del pH por tipo de agua principal",
     x     = NULL,
     y     = "pH (laboratorio)"
   ) +
@@ -31,13 +63,36 @@ guardar_plot(
 # 9.2 Box Plot TEMPERATUR vs CLASIFICACION_LIMPIA
 # Vemos de las temperatura mayores a 0 y que no son mixtas ni compuestas: 
 # (Bicarbonatada / Clorurada / Sulfatada)
+#Hallar percentiles
 
-box2 <- datos %>%
+cat("\n === Resumen del diagrama de caja de Clasificación vs Temperatura ===\n")
+boxplot2_summarise <- datos_top3 %>% 
+  filter (!is.na(CLASIFICACION_LIMPIA), 
+          !is.na(TEMPERATUR),
+          ) %>%
+  group_by(CLASIFICACION_LIMPIA) %>%
+  summarise(
+    
+    Q1 = quantile(TEMPERATUR, 0.25),
+    
+    MEDIAN = quantile(TEMPERATUR, 0.50),
+    
+    Q3 = quantile(TEMPERATUR, 0.75),
+    
+    RIQ = IQR(TEMPERATUR)
+    
+  )
+
+# Impresión de los percentiles del boxplot 2
+
+print(boxplot2_summarise)
+
+# Creación del boxplot2
+
+box2 <- datos_top %>%
   filter(!is.na(CLASIFICACION_LIMPIA),
-         !is.na(TEMPERATUR),
-         TEMPERATUR > 0,
-         CLASIFICACION_LIMPIA %in% top3
-  ) %>%
+         !is.na(TEMPERATUR)
+         ) %>%
   ggplot(aes(x    = CLASIFICACION_LIMPIA,
              y    = TEMPERATUR,
              fill = CLASIFICACION_LIMPIA)) +
