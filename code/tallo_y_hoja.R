f7d86f0ae441ac45cbce07681e9a9637710c344d
z3r0soul <santiagovargas9@outlook.com>
2026-06-03 19:09:43 -0500

    Actualización Filtrado
    En escencia se hizo lo mismo del filtrado de los anteriores commits.

--- code/tallo_y_hoja.R
+++ code/tallo_y_hoja.R
@@ -5,9 +5,20 @@
 # pH — escala = 2 para mostrar un decimal de resolución
 cat("\n══ Tallo y hoja: pH (valores > 0) ══\n")
 
-stem(datos$PH_LABORATORIO, scale = 2)
+# 1. Extraemos los valores de pH válidos como un vector
+ph_manantiales <- datos %>% 
+  filter(!is.na(PH_LABORATORIO)) %>% 
+  pull(PH_LABORATORIO)
+
+# 2. Dibujamos el diagrama de tallo y hojas en la consola
+stem(ph_manantiales, scale = 2)
+
 
 # Temperatura — scale = 1 agrupa en decenas
 cat("\n══ Tallo y hoja: Temperatura (valores > 0) ══\n")
 
-stem(datos$TEMPERATUR, scale = 1)
+temp_manantiales <- datos %>%
+  filter(!is.na(TEMPERATUR)) %>%
+  pull (TEMPERATUR)
+
+stem(temp_manantiales, scale = 1)
